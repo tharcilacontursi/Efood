eb2e7e1ca15338897926fd971361fb9b5fad61ee
Tharcila Contursi <tharcilakcontursi@gmail.com>
2024-08-22 10:59:31 +0200

    corrigindo rota api getRestaurantsList

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,14 +4,14 @@ import { Prato, Restaurante } from '../pages/Home'
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://fake-api-tau.vercel.app/api/efood',
+    baseUrl: 'https://fake-api-tau.vercel.app/api/efood/',
   }),
   endpoints: (builder) => ({
     getFeaturedRestaurant: builder.query<Restaurante, void>({
       query: () => 'restaurante',
     }),
     getRestaurantsList: builder.query<Restaurante[], void>({
-      query: () => '/',
+      query: () => 'restaurantes',
     }),
     getRestaurantDetails: builder.query<Prato[], void>({
       query: () => 'restaurante',
